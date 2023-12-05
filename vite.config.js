d0729301ba44c8cd58f2d8428ada3a9f931aee39
annchana <annchanaoat@gmail.com>
2023-12-05 17:59:25 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://unchana19.github.io/mbti-yours/"
+  base: "/mbti-yours/"
 })
